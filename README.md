4c542ef03d243f716c0ec308ea479dfc63c668bc
José Manuel Aliaga <jose.man.aliaga@dxc.com>
2021-04-19 16:07:09 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# My Own
+
+- npm install particles.js --save
+- npm install animate.css --save
+- <https://sowebsited.com/basics/hamburger.html>
---
+++ js/index.js
@@ -0,0 +1,28 @@
+if (window.outerWidth < 600) {
+    $(function () {
+        let trigger = $('#hamburger');
+        let isClosed = true;
+        
+        // trigger.on("load", function() {
+        //     burgerTime();
+        // })
+
+        trigger.on("click", function () {
+            burgerTime();
+        });
+        
+        function burgerTime() {
+            if (isClosed == true) {
+                trigger.removeClass('is-open');
+                trigger.addClass('is-closed');
+                isClosed = false;
+                $('#respMenu').toggle();
+            } else {
+                trigger.removeClass('is-closed');
+                trigger.addClass('is-open');
+                isClosed = true;
+                $('#respMenu').toggle();
+            }
+        }
+    })
+}
